0d6fcecf4bbc952c706c8dcbb7eda2ea53c53147
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-09-17 12:39:49 -0700

    starting initial structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+
+async function main() {
+    // get the client
+    const mysql = require('mysql2/promise');
+    // create the connection
+    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'test'});
+    // query database
+    const [rows, fields] = await connection.execute('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?', ['Morty', 14]); //This is an example, change this.
+  }
