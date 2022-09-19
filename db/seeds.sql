a18f6fe7a7c19b518f7215263392f638645308cd
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-09-18 20:07:30 -0700

    started seeding

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (id, [name])
+VALUES  (1, "Engineering"),
+        (2, "Sales"),
+        (3, "Finance"),
+        (4, "Legal");
+
+INSERT INTO [role] (id, title, salary, department_id)
+VALUES  (1, "Front_End_Developer", 109576.84,  )
